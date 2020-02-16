ad7c79d4b5360add42449f343386c4b3e85377d7
AnChangNice <anchangnice@gmail.com>
2020-02-17 00:41:45 +0800

    Add source code.

---
+++ mcu_prj/OLED_imageShow/ssd1306/ssd1306_tests.h
@@ -0,0 +1,9 @@
+#ifndef __SSD1306_TEST_H__
+#define __SSD1306_TEST_H__
+
+void ssd1306_TestBorder();
+void ssd1306_TestFonts();
+void ssd1306_TestFPS();
+void ssd1306_TestAll();
+
+#endif // __SSD1306_TEST_H__
