0eb4928de95e4a247d2d208bbc199e1761fa8e12
AnChangNice <anchangnice@gmail.com>
2020-02-22 14:31:26 +0800

    Add new image binarization methods.
    Signed-off-by: AnChangNice <anchangnice@gmail.com>

---
+++ python_gui/common/Singleton.py
@@ -0,0 +1,14 @@
+
+
+def Singleton(cls):
+    """
+        Singleton class
+    """
+    _instance = {}
+
+    def _singleton(*args, **kargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kargs)
+        return _instance[cls]
+
+    return _singleton
